1ef5a6a1715cbab8cff8d5df80fcc4420a0b0c0b
liuw <liuw@jwitsec.com>
2020-08-10 14:42:29 +0800

    config

---
+++ demo7-config-client/src/main/java/demo/DemoConfigClientApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DemoConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoConfigClientApplication.class,args);
+    }
+}
